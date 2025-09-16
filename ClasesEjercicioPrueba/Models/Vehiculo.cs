5be78d3244a67f1b51f9a88568a5c55472f8c8f4
Marianexoo59 <marianoaressi1@gmail.com>
2025-09-16 17:09:48 -0300

    ejercicio extendido

--- ClasesEjercicioPrueba/Models/Vehiculo.cs
+++ ClasesEjercicioPrueba/Models/Vehiculo.cs
@@ -9,7 +9,16 @@
         public int cantidadPuertas { get; set; }
         public string motor { get; set; }
 
+        public int CapacidadCarga { get; set; }
         public bool tieneAuxiliar { get; set; }
+
+        public Vehiculo(string patente, string marca, string Modelo, int CapacidadCarga)
+        {
+            this.patente = patente;
+            this.marca = marca;
+            this.modelo = Modelo;
+            this.CapacidadCarga = CapacidadCarga;
+        }
 
     }
 }
---
+++ ClasesEjercicioPrueba/Models/Viajes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesEjercicioPrueba.Models
+{
+    public class Viajes
+    {
+        public int Id { get; set; }
+        public string Origen {  get; set; }
+        public string Destino { get; set; }
+        public DateTime Fecha { get; set; }
+        public int DistanciaKM { get; set; }
+        public int VehiculoID { get; set; }
+    }
+}
